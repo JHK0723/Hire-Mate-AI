19a0065dda370eee191c4172af16c2c1b6f4e5ea
Sambarlasagna <jayashuriyajayaprakash@gmail.com>
2025-02-11 00:55:31 +0530

    edited scripts1.js

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -56,3 +56,22 @@ function triggerFileUpload() {
             }
         });
     };
+
+    // function generatemail() {
+    // console.log("Button clicked, showing loading screen...");
+
+    // // Show loading screen
+    // document.getElementById("loading-screen").style.display = "flex";
+
+    // fetch("http://127.0.0.1:8000/")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log("Response received:", data);
+    //         document.getElementById("generatemail").innerHTML = `<pre>${data.message1}</pre>`;
+    //         document.getElementById("loading-screen").style.display = "none";
+    //     })
+    //     .catch(error => {
+    //         console.error("Error fetching data:", error);
+    //         document.getElementById("data").innerText = "Failed to load data!";
+    //         document.getElementById("loading-screen").style.display = "none"; // Hide loader on error
+    //     });
--- frontend/scripts1.js
+++ frontend/scripts1.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("retrieve").innerHTML = "<p class='fadein'>Mail preview!</p>";
+    
+     fetch("http://127.0.0.1:8000/preview/")
+        .then(response => response.json())
+        .then(data => {
+            console.log("Response received:", data);
+            document.getElementById("previewmail").innerHTML = `<pre>${data.message1}</pre>`;
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+            document.getElementById("data").innerText = "Failed to load data!";
+        });
 });
+
+
+// function generatemail() {
+//     console.log("Button clicked, showing loading screen...");
+
+//     // Show loading screen
+//     document.getElementById("loading-screen").style.display = "flex";
+
+//     fetch("http://127.0.0.1:8000/")
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log("Response received:", data);
+//             document.getElementById("generatemail").innerHTML = `<pre>${data.message1}</pre>`;
+//             document.getElementById("loading-screen").style.display = "none";
+//         })
+//         .catch(error => {
+//             console.error("Error fetching data:", error);
+//             document.getElementById("data").innerText = "Failed to load data!";
+//             document.getElementById("loading-screen").style.display = "none"; // Hide loader on error
+//         });
